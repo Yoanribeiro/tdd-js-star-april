f166f9fbcd05045985758aa6057f72e21b8314e1
yoanribeiro <yoanribeiro@gmail.com>
2018-04-12 00:08:15 +0200

    Third Red Phase

--- index.spec.js
+++ index.spec.js
@@ -23,6 +23,17 @@ describe("Evens function",()=>{
         expect(result).toEqual(expectedResult);
     });
 
+    test("should return the array with one element if the first element is odd and the second is even",()=>{
+        // given
+        const input = [1,2];
+        const expectedResult = [2];
+        // When
+        const result = evens(input);
+
+        // Then
+        expect(result).toEqual(expectedResult);
+    });
+
 });
 
 describe("Odds function",()=>{
@@ -41,6 +52,17 @@ describe("Odds function",()=>{
         // given
         const input = [1];
         const expectedResult = [1];
+        // When
+        const result = odds(input);
+
+        // Then
+        expect(result).toEqual(expectedResult);
+    });
+
+    test("should return the array with one element if the first element is even and the second is odd",()=>{
+        // given
+        const input = [2,3];
+        const expectedResult = [3];
         // When
         const result = odds(input);
 
