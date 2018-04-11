0a9400f8e9ad7d5bf64bb3475f0c4c944c7814d5
yoanribeiro <yoanribeiro@gmail.com>
2018-04-11 23:37:39 +0200

    Second Green Phase

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 function evens(arr){
+    if(arr.length == 1){
+        if(arr[0] % 2 === 0)
+            return arr;
+    }
     return [];
 }
 
 function odds(arr){
+    if(arr.length == 1){
+        if(arr[0] % 2 !== 0)
+            return arr;
+    }
     return [];
 }
 
