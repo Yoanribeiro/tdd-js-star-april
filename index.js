160c7ddaa28ac9494cc11a1f0e14d4dca8024b11
yoanribeiro <yoanribeiro@gmail.com>
2018-04-12 00:12:05 +0200

    Third Green Phase

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ function evens(arr){
         if(isEven(arr[0]))
             return arr;
     }
+    if(arr.length == 2){
+        if(!isEven(arr[0]) && isEven(arr[1])){
+            const res = [];
+            res.push(arr[1])
+            return res;
+        }
+    }
     return [];
 }
 
@@ -14,6 +21,13 @@ function odds(arr){
     if(arr.length == 1){
         if(!isEven(arr[0]))
             return arr;
+    }
+    if(arr.length == 2){
+        if(isEven(arr[0]) && !isEven(arr[1])){
+            const res = [];
+            res.push(arr[1])
+            return res;
+        }
     }
     return [];
 }
